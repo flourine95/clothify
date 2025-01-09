2ea998ec96e5a17db3da4b1585fe7b6faaf15810
Nguyen Phi Long <22130152@st.hcmuaf.edu.vn>
2025-01-09 15:59:04 +0700

    modifiers properties can nullable

--- clothify-api/Models/User.cs
+++ clothify-api/Models/User.cs
@@ -5,13 +5,13 @@ public class User
     public int Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string Fullname { get; set; }
-    public string Phone { get; set; }
+    public string? Fullname { get; set; }
+    public string? Phone { get; set; }
     public byte Role { get; set; }
     public byte Status { get; set; }
-    public string Avatar { get; set; }
-    public string OauthProvider { get; set; }
-    public string OauthId { get; set; }
+    public string? Avatar { get; set; }
+    public string? OauthProvider { get; set; }
+    public string? OauthId { get; set; }
     public bool EmailVerified { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
